d4b2d56828d478a678e8c92c144b628bb87a67b0
PhanThiNhatLinh <nhatlinhphan9999@gmail.com>
2020-04-27 13:53:39 +0700

    ud2

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(groupproductsTableSeeder::class);
+        // $this->call(groupproductsTableSeeder::class);
         $this->call(GroupproductsTableSeeder::class);
     }
 }
